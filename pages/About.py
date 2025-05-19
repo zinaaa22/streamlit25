9645ca26f2ec37913ef600845726d81e8a965d32
Kim jina <3636wlsdk@gmail.com>
2025-05-19 12:43:50 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.write("This app is under construction.")
+st.write("This app is under construction. This is my favorite food.")
 
 url="https://raw.githubusercontent.com/zinaaa22/streamlit25/main/IMG_0112.jpeg"
 st.image(url, caption="Image link", width=400)  # width in pixels
